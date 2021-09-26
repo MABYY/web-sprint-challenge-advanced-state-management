0aaf16705a2a624fe710db3420f8ac9b46016d1b
Maria Negri <mariamnegri@gmail.com>
2021-09-26 20:16:08 -0300

    Final Push

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from "react-dom";
 
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import logger from 'redux-logger';
 import thunk from 'redux-thunk'; // we use thunk to 
 import reducer  from './reducers/index';
 
